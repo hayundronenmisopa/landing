d9b574f2f0f4b371ac5fd1f2b3c4c192bdd8b623
jayroguerrs <jguerreros@pucp.edu.pe>
2025-01-30 14:23:05 -0500

    cambios

--- src/app/pages/inicio/inicio.component.ts
+++ src/app/pages/inicio/inicio.component.ts
@@ -1,13 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
+import { CommonModule } from '@angular/common'; // Asegúrate de importar CommonModule
 
 @Component({
     selector: 'inicio',
     standalone: true,
     imports: [
+      CommonModule
     ],
     templateUrl: './inicio.component.html',
     styleUrl: './inicio.component.scss'
 })
 export class InicioComponent {
-    title = 'hayundronenmisopa';
+    isScrolled = false; // Variable para detectar si el usuario ha hecho scroll
+
+    @HostListener('window:scroll', [])
+    onWindowScroll() {
+        this.isScrolled = window.scrollY > 50;
+    }
 }
